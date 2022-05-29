755ed80f065cd189f4406c055c87b1e356bf567d
Leogms47 <leogms063@gmail.com>
2022-05-29 19:04:31 -0300

    Modulo iniciado

---
+++ Produtos_Estoques(Construtores)/Produtos_Estoques/Produto.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Produtos_Estoques
+{
+    class Produto
+    {
+        //Atributos da Classe
+        public string Nome;
+        public double Preco;
+        public int Quantidade;
+
+        //Construtores
+        public Produto(string nome, double preco, int quantidade)
+        {
+            Nome = nome;
+            Preco = preco;
+            Quantidade = quantidade;
+        }
+
+        //Este método retornar a multiplicação do preço x quantidade
+        public double ValorTotalEmEstoque()
+        {
+            return Preco * Quantidade;
+        }
+
+        //Este método retornar a formatação de string concatenados com seus respectivos valores
+        public override string ToString()
+        {
+            return "Produto: " + Nome + ", Preço: R$ " + Preco.ToString("F2") + ", Quantidade: " + Quantidade + ", Total: R$ " + ValorTotalEmEstoque().ToString("F2");
+        }
+
+        //Este método faz com que o atributo Quantidade recebe ela mesma + quantidade que é o parâmetro de entrada
+        public void AdicionarProdutos(int quantidade)
+        {
+            Quantidade = Quantidade + quantidade;
+        }
+
+        //Este método faz com que o atributo Quantidade recebe ela mesma - quantidade que é o parâmetro de entrada
+        public void RemoverProdutos(int quantidade)
+        {
+            Quantidade = Quantidade - quantidade;
+        }
+    }
+}
---
+++ Produtos_Estoques(Construtores)/Produtos_Estoques/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Produtos_Estoques
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {       
+            
+
+            Console.WriteLine("Informe os dados do produto: ");
+            Console.Write("Nome: ");
+            string nome = Console.ReadLine().ToString();
+            Console.Write("Preço: ");
+            double preco = double.Parse(Console.ReadLine());
+            Console.Write("Quantidade no estoque: ");
+            int quantidade = int.Parse(Console.ReadLine());
+
+            Produto p = new Produto(nome, preco, quantidade);
+
+            Console.WriteLine();
+            Console.WriteLine("Dados do Produto: " + p);
+
+            Console.WriteLine();
+            Console.Write("Digite o número de produtos a ser adicionado ao estoque: ");
+            int qte = int.Parse(Console.ReadLine());
+            
+            /*O método p.AdicionarProdutos é capaz de receber um valor de entrada da variavel qte é 
+            armazenar o valor dessa variável na variavel de parâmetro int quantidade e efetuar a operação*/
+            p.AdicionarProdutos(qte);
+
+            Console.WriteLine();
+            Console.WriteLine("Dados Atualizados: " + p);
+
+            Console.WriteLine();
+            Console.Write("Digite o número de produtos a ser removido do estoque: ");
+            qte = int.Parse(Console.ReadLine());
+
+            p.RemoverProdutos(qte);
+
+            Console.WriteLine();
+            Console.WriteLine("Dados Atualizados: " + p);
+
+        }
+    }
+}
